4037fb654687c1508076f4c2229e719f0a679fa1
Nabeel Naeem <nnabeel03@gmail.com>
2026-09-11 13:27:26 +0500

    fixed the breaking code

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
           </p>
         </div>
         <button type="button" className="counter" onClick={() => setCount((count) => count + 1)}>
-          Counter: {count}
+          Count is {count}
         </button>
       </section>
 
